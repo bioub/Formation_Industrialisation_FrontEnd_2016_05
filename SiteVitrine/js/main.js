e4372165810d89e10cbaabfdd81eaa5c3264a052
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2016-05-25 12:54:27 +0100

    Correction du bug, validation au submit

--- SiteVitrine/js/main.js
+++ SiteVitrine/js/main.js
@@ -2,4 +2,8 @@
 import valider from './validator';
 
 let form = document.querySelector('#myModal form');
-valider(form);
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    valider(form);
+});
